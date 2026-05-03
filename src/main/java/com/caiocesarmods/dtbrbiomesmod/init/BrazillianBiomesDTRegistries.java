491d8f4e6a28897bf0ecc7d0862a23a251144882
ADM Caio <caiodnobre@gmail.com>
2026-05-03 15:18:07 -0300

    Initial commit

--- src/main/java/com/caiocesarmods/dtbrbiomesmod/init/BrazillianBiomesDTRegistries.java
+++ src/main/java/com/caiocesarmods/dtbrbiomesmod/init/BrazillianBiomesDTRegistries.java
@@ -2,6 +2,7 @@ package com.caiocesarmods.dtbrbiomesmod.init;
 
 import com.caiocesarmods.dtbrbiomesmod.BrazillianBiomesDTAddon;
 import com.caiocesarmods.dtbrbiomesmod.Trees.Family.CashewFamily;
+import com.caiocesarmods.dtbrbiomesmod.Trees.Family.IpeFamily;
 import com.caiocesarmods.dtbrbiomesmod.Trees.Family.KapokFamily;
 import com.ferreusveritas.dynamictrees.api.registry.TypeRegistryEvent;
 import com.ferreusveritas.dynamictrees.trees.Family;
@@ -15,6 +16,7 @@ public class BrazillianBiomesDTRegistries {
     public static void registerFamilyTypes(final TypeRegistryEvent<Family> event) {
         event.registerType(BrazillianBiomesDTAddon.resLoc("kapok"), KapokFamily.TYPE);
         event.registerType(BrazillianBiomesDTAddon.resLoc("cashew"), CashewFamily.TYPE);
+        event.registerType(BrazillianBiomesDTAddon.resLoc("ipe"), IpeFamily.TYPE);
 
     }
 }
