dd5ecc052505ac7ba4b1fbb0597e4f719284cd42
ADM Caio <caiodnobre@gmail.com>
2026-05-08 21:34:09 -0300

    Initial commit

--- src/main/java/com/caiocesarmods/dtbrbiomesmod/init/BrazillianBiomesDTRegistries.java
+++ src/main/java/com/caiocesarmods/dtbrbiomesmod/init/BrazillianBiomesDTRegistries.java
@@ -1,24 +1,17 @@
 package com.caiocesarmods.dtbrbiomesmod.init;
 
 import com.caiocesarmods.dtbrbiomesmod.BrazillianBiomesDTAddon;
-import com.caiocesarmods.dtbrbiomesmod.Trees.Family.CashewFamily;
-import com.caiocesarmods.dtbrbiomesmod.Trees.Family.IpeFamily;
-import com.caiocesarmods.dtbrbiomesmod.Trees.Family.KapokFamily;
 import com.ferreusveritas.dynamictrees.api.registry.TypeRegistryEvent;
 import com.ferreusveritas.dynamictrees.trees.Family;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class BrazillianBiomesDTRegistries {
 
     @SubscribeEvent
     public static void registerFamilyTypes(final TypeRegistryEvent<Family> event) {
-        event.registerType(BrazillianBiomesDTAddon.resLoc("kapok"), KapokFamily.TYPE);
-        event.registerType(BrazillianBiomesDTAddon.resLoc("cashew"), CashewFamily.TYPE);
-        event.registerType(BrazillianBiomesDTAddon.resLoc("ipe"), IpeFamily.TYPE);
+
 
     }
 
